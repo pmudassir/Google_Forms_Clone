ff6749b877b2a8adc98e2be6c66ae2af65eb4c4b
pmudassir <pmuddasir@gmail.com>
2023-08-05 16:03:17 +0530

    header component completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
     <div>
-      <h1>Hi from react</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './header.css'
+import formImage from '../../images/forms.png'
+import { Avatar, IconButton } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AppsIcon from '@mui/icons-material/Apps';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header_info">
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+                <img src={formImage} style={{ height: '40px', width: '40px' }} className="form_image" alt="forms logo" />
+                <div className="info">Forms</div>
+            </div>
+            <div className="header_search">
+                <IconButton>
+                    <SearchIcon />
+                </IconButton>
+                <input type="text" placeholder='Search' name='search' />
+            </div>
+            <div className="header_right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <Avatar />
+                </IconButton>
+            </div>
+        </div>
+    )
+}
+
+export default Header
